22c7fdefda0fe4c1be5870f191646950adebe4ad
Stephanie Messad <sabud1208@gmail.com>
2018-11-26 04:16:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ array = ["Kiwi", "durianuiiu", "starfruit"]
 def oxford_comma(array)
 if array.length == 2
 return  "#{array[0]} and #{array[1]}"
-elsif 2 > array.length
+elsif 2 < array.length
   "#{array[-1]}".insert(0, " and")
 end
 array.join(" , ")
