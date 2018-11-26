1f599012860d8ac39ec94c97d47ffb5855a63e72
Stephanie Messad <sabud1208@gmail.com>
2018-11-26 03:23:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
-"Kiwi", "durian", "starfruit".split
 
 
-def oxford_comma("Kiwi", "durian", "starfruit".split)
- results= array.join(" and ")
+
+def oxford_comma
+ ("Kiwi", "durian", "starfruit").to_a
 end
