6e9df052a019899615bf6ba723c08881b9209f2b
Stephanie Messad <sabud1208@gmail.com>
2018-11-26 04:19:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,12 @@
-array= ["Kiwi", "durian", "starfruit"]
+
+array = ["Kiwi", "durianuiiu", "starfruit"]
 
 
 def oxford_comma(array)
- results= array.join(" , " && " and ") 
+if array.length == 2
+return  "#{array[0]} and #{array[1]}"
+elsif 2 < array.length
+  array[-1].insert(0, "and ")
+end
+array.join(", ")
 end
